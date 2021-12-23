851f6bf2c62508df50f187fcf24f85dde02210e2
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-23 18:48:09 -0500

    second commit

---
+++ packages/modern-christian/src/components/list.js
@@ -0,0 +1,25 @@
+// File: /packages/my-first-theme/src/components/list.js
+
+import React from "react"
+import { connect } from "frontity"
+import Link from "@frontity/components/link" // addin
+
+const List = ({ state }) => {
+  const data = state.source.get(state.router.link)
+
+  return (
+    <div>
+      {data.items.map((item) => {
+        const post = state.source[item.type][item.id]
+        return (
+          <Link key={item.id} link={post.link}>
+            {post.title.rendered}
+            <br />
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default connect(List)
---
+++ packages/modern-christian/src/index.js
@@ -0,0 +1,18 @@
+// File: /packages/my-first-theme/src/index.js
+
+import Root from "./components"
+
+const myFirstTheme = {
+  name: "modern-christian",
+  roots: {
+    theme: Root,
+  },
+  state: {
+    theme: {},
+  },
+  actions: {
+    theme: {},
+  },
+}
+
+export default myFirstTheme
