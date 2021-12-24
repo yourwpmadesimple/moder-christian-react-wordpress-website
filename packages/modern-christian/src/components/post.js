42a17e23053615a89c85ba22e817c10bb8b7d1e8
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-23 19:53:31 -0500

    imported dayjs, formatted date for posts

--- packages/modern-christian/src/components/post.js
+++ packages/modern-christian/src/components/post.js
@@ -2,16 +2,19 @@
 
 import React from "react"
 import { connect } from "frontity"
+import dayjs from "dayjs"
 
 const Post = ({ state }) => {
   const data = state.source.get(state.router.link)
   const post = state.source[data.type][data.id]
   const author = state.source.author[post.author]
 
+  const formattedDate = dayjs(post.date).format("MMM d, YYYY")
+
   return (
-    <div>
+    <div> 
       <h2>{post.title.rendered}</h2>
-      <p><strong>Posted: </strong> {post.date} </p>
+      <p><strong>Posted: </strong> {formattedDate} </p>
       <p><strong>Author: </strong> {author.name} </p>
       <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
     </div>
