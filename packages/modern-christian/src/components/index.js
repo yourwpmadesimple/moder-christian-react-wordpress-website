5bbf7a729ea9839e25f73dbc9c118f5726a8d030
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-23 20:21:59 -0500

    added styles with styled components using Global, css, and styled from frontity

--- packages/modern-christian/src/components/index.js
+++ packages/modern-christian/src/components/index.js
@@ -1,7 +1,7 @@
 // File: /packages/my-first-theme/src/components/index.js
 
 import React from "react"
-import { connect, Global, css } from "frontity"
+import { connect, Global, css, styled } from "frontity"
 import Link from "@frontity/components/link"
 import Switch from "@frontity/components/switch"
 
@@ -15,13 +15,21 @@ const Root = ({ state }) => {
   return (
     <>
     <Global styles={css`
+    *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      
+    }
     html{
       font-family: system-ui, Verdana, Arial, sas-serif
     }
     `}
     />
     {/* ... */}
-      <h1>Hello Frontity</h1>
+    <Header>
+    <HeaderContent>
+    <h1>Hello Frontity</h1>
       <p>Current URL: {state.router.link}</p>
       <nav>
         <Link link="/">Home</Link>
@@ -30,16 +38,55 @@ const Root = ({ state }) => {
         <br />
         <Link link="/about-us">About Us</Link>
       </nav>
+      </HeaderContent>
+    </Header>
+      
       <hr />
-      <main>
+      <Main>
       <Switch>
       <List when={data.isArchive} />
       <Post when={data.isPost} />
       <Page when={data.isPage} />
     </Switch>
-      </main>
+      </Main>
     </>
   )
 }
 
 export default connect(Root)
+
+const Header = styled.header`
+  background-color: #e5edee;
+  border-width: 0 0 8px 0;
+  border-style: solid;
+  border-color: maroon;
+
+  h1 {
+    color: #4a4a4a;
+  }
+  `
+  const HeaderContent = styled.div`
+  max-width: 800px;
+  padding: 2em 1em;
+  margin: auto;
+`
+const Main = styled.main`
+  max-width: 800px;
+  padding: 1em;
+  margin: auto;
+
+  img {
+    max-width: 100%;
+  }
+  h2 {
+    margin: 0.5em 0;
+  }
+  p {
+    line-height: 1.25em;
+    margin-bottom: 0.75em;
+  }
+  figcaption {
+    color: #828282;
+    font-size: 0.8em;
+    margin-bottom: 1em;
+  }`
